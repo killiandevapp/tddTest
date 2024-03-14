396d7a90dd8b03f83ad1a729934b60688f66519d
Kilian <killiandevapp@gmail.com>
2024-03-14 11:02:22 +0100

    not final 1.5

--- main.js
+++ main.js
@@ -2,22 +2,15 @@ export function add(str) {
     if(str.length === 0){
         return 0
     }
-    if(str === "1"){
-        return 1;
-    }
     if(str < 0){
         return `Negatves not allowed for ${str}`
     }
-
-
     let delimiter = /[,\n]/;
     if (str.startsWith("//")) {
         const customDelimite = str[2]; 
         delimiter = new RegExp(customDelimite.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')); 
         str = str.substring(4); 
     }
-
-
     const nums = str.split(delimiter);
     if (nums.includes("")) {
         return null; 
