9b02d0ff44400c52c5dedcbca1895641d89310d4
Kilian <killiandevapp@gmail.com>
2024-03-14 10:16:59 +0100

    test passed 1.4

--- testing/main.test.js
+++ testing/main.test.js
@@ -1,7 +1,7 @@
 import { expect, test } from 'vitest'
 import { add } from '../main'
 
-test('Empty string returns 0', () => {
+test('Empty string return 0', () => {
     expect(add("")).toBe(0);
 });
 test('Empty string returns 1', () => {
@@ -12,15 +12,15 @@ test('two numbers', () => {
     expect(add("1,2")).toBe(3);
 });
 
-test('Multiple numbers string returns their sum', () => {
+test('Multiple numbers string return their sum', () => {
     expect(add("1,2,3,4,5")).toBe(15);
 });
-test('Newline as delimiter returns correct sum', () => {
+test('newline as delimiter returns correct sum', () => {
     expect(add("1\n2,3")).toBe(6);
     expect(add("1,\n")).toBe(null);
 });
 
-test('Support different delimiters', () => {
+test('Support different delimiter', () => {
     expect(add("//;\n1;2")).toBe(3);
 
 });
